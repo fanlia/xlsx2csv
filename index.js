a57aa6743108de6063ee87e6ce458d9e99ca8d79
fanlia <you@example.com>
2022-07-16 07:39:22 +0800

    first commit

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ async function parse_sheet(xml, texts, formats, callback) {
                     text = ''
                 } else {
                     if (typeof numFormat === 'string') {
-                        const isDate = /d/.test(numFormat) && numfmt.isDate(numFormat)
+                        const isDate = /[yd]/.test(numFormat) && numfmt.isDate(numFormat)
                         if (isDate) {
                             text = formatDate(value)
                         } else {
