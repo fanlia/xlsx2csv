464e6621c519caa38eb259ed4958efccf318f9a8
fanlia <you@example.com>
2022-07-16 08:42:38 +0800

    first commit

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,5 @@ type InputType = ArrayBuffer | Buffer;
 
 type CallbackType = (data: Array<string>) => void;
 
-export default function xlsx2csv(input: InputType, callback: CallbackType) : void;
+export default function xlsx2csv(input: InputType, callback?: CallbackType) : void;
 
